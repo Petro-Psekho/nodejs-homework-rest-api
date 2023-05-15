a58523b8077184e7ee552adc7789c2881caff408
Petro Psekho <psekho.pp@gmail.com>
2023-05-15 12:17:31 +0300

    install mongodb

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,9 @@
 const contacts = require("../models/contacts");
 
+const mongoose = require("mongoose");
+
+mongoose.connect();
+
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
