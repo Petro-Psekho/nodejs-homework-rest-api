f0458ce3c6a859aff05fb47d028177a56a5cdd17
Petro Psekho <psekho.pp@gmail.com>
2023-05-15 15:31:18 +0300

    move DB_HOST to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const DB_HOST =
+  "mongodb+srv://petro:Smart5791@cluster0.lxdixxc.mongodb.net/contacts-db?retryWrites=true&w=majority";
+
+module.exports = {
+  DB_HOST,
+};
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const app = require("./app");
 
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://petro:Smart5791@cluster0.lxdixxc.mongodb.net/contacts-db?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
 mongoose.set("strictQuery", true);
 
@@ -11,12 +10,9 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
+    console.log("=== Database connection successful ===");
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
