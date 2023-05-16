2a72a523f3a03b083932b3f72e0da1ea7176209c
Petro Psekho <psekho.pp@gmail.com>
2023-05-16 16:22:18 +0300

    add Schema and model

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,16 +9,16 @@ const router = express.Router();
 
 router.get("/", ctrl.getAllContacts);
 
-router.get("/:contactId", ctrl.getContactById);
+// router.get("/:contactId", ctrl.getContactById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+// router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:contactId", ctrl.deleteContactById);
+// router.delete("/:contactId", ctrl.deleteContactById);
 
-router.put(
-  "/:contactId",
-  validateBody(schemas.addSchema),
-  ctrl.updateContactById
-);
+// router.put(
+//   "/:contactId",
+//   validateBody(schemas.addSchema),
+//   ctrl.updateContactById
+// );
 
 module.exports = router;
