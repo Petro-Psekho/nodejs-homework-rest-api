98e4fc986a016eb0933f4067bf25efb5377dfba6
Petro Psekho <psekho.pp@gmail.com>
2023-05-21 19:02:33 +0300

    add pagination

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,7 +3,12 @@ const { ctrlWrapper } = require('../../helpers');
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }, '-createdAt -updatedAt');
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, '-createdAt -updatedAt', {
+    skip,
+    limit,
+  }).populate('owner', 'email');
   res.json(result);
 };
 
