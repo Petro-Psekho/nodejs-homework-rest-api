687b27421898124153e67ca132812bd486043bea
Petro Psekho <psekho.pp@gmail.com>
2023-05-21 20:39:42 +0300

    add filter favorite

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,11 +3,13 @@ const { ctrlWrapper } = require('../../helpers');
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite = true } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, '-createdAt -updatedAt', {
+
+  const result = await Contact.find({ owner, favorite }, '-createdAt -updatedAt', {
     skip,
     limit,
+    favorite,
   }).populate('owner', 'email');
   res.json(result);
 };
